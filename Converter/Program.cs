35fca74c64c4fe14d729b4f12e8e820ca4ede49c
Andrey Valchuk <immigrationimmigrant@gmail.com>
2021-02-28 20:19:17 +0300

    Add project files.

---
+++ Converter/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConverterLibrary;
+using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main()
+        {
+            uint valueToConvert;
+
+            Console.Write("Введите неотрицательное целое десятичное число: ");
+
+            while (!UInt32.TryParse(Console.ReadLine(), out valueToConvert))
+            {
+                Console.Write("Неверный ввод. Попробуйте еще: ");
+            }
+
+            Console.WriteLine("Данное число в двоичном формате (получено с использованием библиотечный методов конвертирования): ");
+            Console.WriteLine(CustomConverter.ConvertToBinaryString(valueToConvert));
+
+            Console.WriteLine("Данное число в двоичном формате (получено без использования библиотечных методов конвертирования ): ");
+            Console.WriteLine(CustomConverter.ConvertToBinaryStringCustom(valueToConvert));
+        }
+    }
+}
---
+++ ConverterLibrary/CustomConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConverterLibrary
+{
+    public static class CustomConverter
+    {
+        public static string ConvertToBinaryString(uint valueToConvert)
+        {
+            return Convert.ToString(valueToConvert, 2);
+        }
+
+        public static string ConvertToBinaryStringCustom(uint valueToConvert)
+        {
+            if (valueToConvert == 0)
+            {
+                return "0";
+            }
+
+            StringBuilder tmp = new StringBuilder();
+
+            while (valueToConvert != 1)
+            {
+                tmp.Append(valueToConvert % 2);
+                valueToConvert /= 2;
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            result.Append('1');
+
+            for (int i = tmp.Length - 1; i >= 0; i--)
+            {
+                result.Append(tmp[i]);
+            }
+
+            return result.ToString();     
+        }
+    }
+}
